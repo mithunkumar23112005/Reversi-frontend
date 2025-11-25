8d4e28594fd81a24ecc461fd8683bb7f520c9943
mithunkumar23112005 <mithunkumarg2005@gmail.com>
2025-11-26 02:09:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import io from 'socket.io-client';
 import logo from "./logo.png";
 
 
-const API_URL = 'https://reversi-djpc.onrender.com';
-const SOCKET_URL = 'https://reversi-djpc.onrender.com';
+const API_URL = 'https://reversi-kb0q.onrender.com';
+const SOCKET_URL = 'https://reversi-kb0q.onrender.com';
 
 let socket;
 
